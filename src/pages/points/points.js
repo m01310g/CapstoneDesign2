55b3d4ca978cc15e1c6a318ec64c54fbe35f6b80
kjmg7979 <kjmg7979@naver.com>
2024-09-24 17:19:55 +0900

    feat: 포인트 충전 페이지

---
+++ src/pages/points/points.js
@@ -0,0 +1,9 @@
+const chargeBtn = document.querySelector("#charge");
+const exchangeBtn = document.querySelector("#exchange");
+
+function handleBtnClick() {
+    console.log("clicked");
+}
+
+chargeBtn.addEventListener("click", handleBtnClick);
+exchangeBtn.addEventListener("click", handleBtnClick);
