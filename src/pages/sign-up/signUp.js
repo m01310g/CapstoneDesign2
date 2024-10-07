6c35eb35730b24a6a25aeaf9596369ac6ba1c122
hha9803 <hha9803@naver.com>
2024-10-07 16:47:15 +0900

    feat: 이메일로 수신한 인증 번호 입력 제한 시간 출력

--- src/pages/sign-up/signUp.js
+++ src/pages/sign-up/signUp.js
@@ -1,4 +1,7 @@
 const $btnClose = document.querySelector(".btn-close");
+const $btnEmailAuthn = document.querySelector("#send-authn");
+const $emailAuthn = document.querySelector("input[name='email-authn']");
+let authnTimer = 180; // 이메일 인증 번호 입력 제한 시간
     
 $btnClose.addEventListener("click", () => {
   window.location.href = "../login/login.html";
@@ -11,3 +14,15 @@ const insertHyphen = (t) => {
     .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3")
     .replace(/(\-{1,2})$/g, "");
 };
+
+// 이메일로 인증번호 전송하면 -> 인증번호 입력 창에 입력 제한 시간 출력
+$btnEmailAuthn.addEventListener("click", () => {
+  const authnIntervalId = setInterval(() => {
+    $emailAuthn.value = `${Math.floor(authnTimer / 60)}:${authnTimer % 60}`;
+    authnTimer--;
+    if(authnTimer === 0) {
+      clearInterval(authnIntervalId);
+      $emailAuthn.value = "인증번호 만료";
+    }
+  }, 1000);
+});
