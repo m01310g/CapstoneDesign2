2b20b7459670c322d7a9d14d1aec0a350696bd60
m01310g <m01310g@gmail.com>
2024-10-10 16:15:33 +0900

    feat: 택시 게시판 검색창 클릭 이벤트 수정

---
+++ src/pages/post/taxi.js
@@ -0,0 +1,18 @@
+// const departure = document.querySelector("#departure");
+// const destination = document.querySelector("#destination");
+// const listFrame = document.querySelector(".list-frame");
+// const confirm = document.querySelector("#confirm-btn");
+
+// const HIDDEN_CLASS_NAME = "hidden";
+
+// departure.addEventListener("click", () => {
+//     listFrame.classList.toggle(HIDDEN_CLASS_NAME);
+// });
+
+// destination.addEventListener("click", () => {
+//     listFrame.classList.toggle(HIDDEN_CLASS_NAME);
+// });
+
+// confirm.addEventListener("click", () => {
+//     listFrame.classList.toggle(HIDDEN_CLASS_NAME);
+// });
